0faa92be6e52f198cf64568cbdef8d5963c8b198
André Carvalho <afacarvalho@yahoo.com.br>
2021-10-13 19:39:41 -0300

    Improves on first definitions

--- cluster3/start/first-step.sh
+++ cluster3/start/first-step.sh
@@ -5,12 +5,21 @@
 # All these steps are described in:
 # https://docs.portainer.io/v/ce-2.9/advanced/reverse-proxy/nginx#deploying-in-a-docker-swarm-scenario
 
-# First, create two networks:
-docker network create -d overlay proxy
-docker network create -d agent_network
+# remove olds
+if [[ "$1" = "rm" ]];then
+  docker stack rm portainer
+  docker volume rm portainer_data
+  docker network rm proxy
+  docker network rm agent_network
+else
 
-# Next, create the volume:
-docker volume create portainer_data
+  # First, create two networks:
+  docker network create -d overlay proxy
+  docker network create -d overlay --attachable agent_network
 
-# using an yaml file to startup Portainer stack
-docker stack deploy portainer -c portainer.yaml
+  # Next, create the volume:
+  docker volume create portainer_data
+
+  # using an yaml file to startup Portainer stack
+  docker stack deploy portainer -c portainer.yaml
+fi;
