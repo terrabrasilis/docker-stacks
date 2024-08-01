b7d4367d41bc9258faff9bc7116917ec639f9e7d
andre-carvalho <afacarvalho@yahoo.com.br>
2024-08-01 11:56:48 -0300

    Chnages to improve the initial step of new cluster

--- secondary-cluster/start/first-step.sh
+++ secondary-cluster/start/first-step.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # Before running this script, make sure all preconditions are met.
-# Get the preconditions in the README of the cluster3 directory
+# Get the preconditions in the README file
 
 # All these steps are described in:
-# https://docs.portainer.io/v/ce-2.9/advanced/reverse-proxy/nginx#deploying-in-a-docker-swarm-scenario
+# https://docs.portainer.io/v/2.20/start/install-ce/server/swarm
 
 # remove olds
 if [[ "$1" = "rm" ]];then
   docker stack rm portainer
-  docker volume rm portainer_data
   docker network rm proxy
   docker network rm backend
   docker network rm agent_network
@@ -18,9 +17,6 @@ else
   docker network create -d overlay proxy
   docker network create -d overlay backend
   docker network create -d overlay --attachable agent_network
-
-  # Next, create the volume:
-  docker volume create portainer_data
 
   # using an yaml file to startup Portainer stack
   docker stack deploy portainer -c portainer.yaml
