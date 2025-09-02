7b854a8c0c9748138a7bbffbff2768cd16de7f82
André Carvalho <afacarvalho@yahoo.com.br>
2025-09-02 10:14:22 -0300

    Change commands to wait for the process to complete

--- terrabrasilis-cluster/start/manager-stacks-secondary-cluster.sh
+++ terrabrasilis-cluster/start/manager-stacks-secondary-cluster.sh
@@ -7,10 +7,10 @@ if [[ "$1" = "rm" ]]; then
     for STACK_NAME in `docker stack ls |tail +2 | awk {'print $1'}`
     do
         echo "removing the stack: ${STACK_NAME}"
-        docker stack rm ${STACK_NAME}
+        docker stack rm --detach=false ${STACK_NAME}
     done
 
-    sleep 45
+    sleep 10
     docker network prune -f
     docker volume prune -f
 
@@ -32,20 +32,20 @@ else
     sleep 10
 
     # the security stack has a keycloak application that does not work on a read-only database, so we keep that stack disabled for now.
-    # docker stack deploy security --compose-file ${STACK_PATH}/stacks/security.yaml --detach=true
+    # docker stack deploy security --compose-file ${STACK_PATH}/stacks/security.yaml --detach=false
 
-    docker stack deploy sgdb --compose-file ${STACK_PATH}/stacks/sgdb.yaml --detach=true
-    docker stack deploy geoserver-cluster --compose-file ${STACK_PATH}/stacks/geoserver-cluster.yaml --detach=true
-    docker stack deploy metadata-app --compose-file ${STACK_PATH}/stacks/metadata-app.yaml --detach=true
-    docker stack deploy webapps --compose-file ${STACK_PATH}/stacks/webapps.yaml --detach=true
-    docker stack deploy webapps-homologation --compose-file ${STACK_PATH}/stacks/webapps-homologation.yaml --detach=true
-    docker stack deploy apis --compose-file ${STACK_PATH}/stacks/apis.yaml --detach=true
-    docker stack deploy homepage-app --compose-file ${STACK_PATH}/stacks/homepage-app.yaml --detach=true
-    docker stack deploy webservers-homologation --compose-file ${STACK_PATH}/stacks/webservers-homologation.yaml --detach=true
-    docker stack deploy portainer --compose-file ${STACK_PATH}/start/portainer.yaml --detach=true
+    docker stack deploy sgdb --compose-file ${STACK_PATH}/stacks/sgdb.yaml --detach=false
+    docker stack deploy geoserver-cluster --compose-file ${STACK_PATH}/stacks/geoserver-cluster.yaml --detach=false
+    docker stack deploy metadata-app --compose-file ${STACK_PATH}/stacks/metadata-app.yaml --detach=false
+    docker stack deploy webapps --compose-file ${STACK_PATH}/stacks/webapps.yaml --detach=false
+    docker stack deploy webapps-homologation --compose-file ${STACK_PATH}/stacks/webapps-homologation.yaml --detach=false
+    docker stack deploy apis --compose-file ${STACK_PATH}/stacks/apis.yaml --detach=false
+    docker stack deploy homepage-app --compose-file ${STACK_PATH}/stacks/homepage-app.yaml --detach=false
+    docker stack deploy webservers-homologation --compose-file ${STACK_PATH}/stacks/webservers-homologation.yaml --detach=false
+    docker stack deploy portainer --compose-file ${STACK_PATH}/start/portainer.yaml --detach=false
 
     # waiting for the services to be fully started before starting the nginx proxy
     sleep 60
-    docker stack deploy webservers --compose-file ${STACK_PATH}/stacks/webservers.yaml --detach=true
+    docker stack deploy webservers --compose-file ${STACK_PATH}/stacks/webservers.yaml --detach=false
 
 fi
